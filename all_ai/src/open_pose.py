f833950634b203a6807acf52985ecba4928e1de0
Ksuck <pongchuti4@gmail.com>
2024-03-27 03:01:51 +0700

    create deth distance mm and pose human

---
+++ all_ai/src/open_pose.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+
+import rospy
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+import cv2
+import threading
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+import numpy as np
+from std_msgs.msg import String, Int32
+
+class HandGestureListener:
+    def __init__(self):
+        rospy.init_node('hand_gesture_listener', anonymous=True)
+        rospy.Subscriber("/camera/color/image_raw", Image, self.rgb_callback)
+        rospy.Subscriber("/camera/depth/image_raw", Image, self.depth_callback)
+
+        self.side_pub = rospy.Publisher("/human_side", String, queue_size = 10)
+        self.dist_pub = rospy.Publisher("/human_dist", Int32, queue_size = 10)
+
+        #pose
+        self.mp_pose = mp.solutions.pose
+        self.pose = self.mp_pose.Pose(static_image_mode=False, min_detection_confidence=0.5, min_tracking_confidence=0.5)
+
+        #cv
+        self.bridge = CvBridge()
+        self.lock = threading.Lock()
+
+        self.rate = rospy.Rate(500)
+
+    def rgb_callback(self, data):
+        try:
+            frame_rgb = self.bridge.imgmsg_to_cv2(data, "bgr8")
+            self.lock.acquire()
+            self.frame_rgb = frame_rgb
+            self.lock.release()
+        except Exception as e:
+            rospy.logerr(e)
+
+    def depth_callback(self, data):
+        try:
+            frame_depth = self.bridge.imgmsg_to_cv2(data, desired_encoding="passthrough")
+            self.lock.acquire()
+            self.frame_depth = frame_depth
+            self.lock.release()
+        except Exception as e:
+            rospy.logerr(e)
+
+    def show_images(self):
+        while not rospy.is_shutdown():
+            if hasattr(self, 'frame_rgb') and hasattr(self, 'frame_depth'):
+                try:
+                    self.lock.acquire()
+                    results = self.pose.process(self.frame_rgb)
+                    if results.pose_landmarks:
+                        mp_drawing = mp.solutions.drawing_utils
+                        mp_drawing.draw_landmarks(
+                        self.frame_rgb, results.pose_landmarks, self.mp_pose.POSE_CONNECTIONS)
+
+                        nose_landmark = results.pose_landmarks.landmark[0]
+                        height, width, _ = self.frame_rgb.shape
+                        cx, cy = int(nose_landmark.x * width), int(nose_landmark.y * height)
+                        cv2.circle(self.frame_rgb, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
+                        cv2.putText(self.frame_rgb, f"{0}", (cx, cy), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
+
+                        str_data = "midle"
+                        str_data = "left" if cx > (width * 2 / 3) else str_data
+                        str_data = "midle" if cx <= (width * 2 / 3) and cx >= (width / 3) else str_data
+                        str_data = "right" if cx < (width / 3) else str_data
+                        
+                        rospy.loginfo(str_data)
+                        self.side_pub.publish(str_data)
+
+                        try:
+                            dist =  self.frame_depth[cy, cx]
+                            print(dist)
+                            self.dist_pub.publish(dist)
+                        except Exception as e:
+                            rospy.logerr(e)                                    
+                        
+
+                    cv2.imshow("RGB", self.frame_rgb)
+                    cv2.imshow("Depth", self.frame_depth)
+                    self.lock.release()
+                    cv2.waitKey(1)
+                except Exception as e:
+                    rospy.logerr(e)
+            self.rate.sleep()
+
+    def run(self):
+        threading.Thread(target=self.show_images).start()
+        rospy.spin()
+
+if __name__ == '__main__':
+    hand_gesture_listener = HandGestureListener()
+    hand_gesture_listener.run()
