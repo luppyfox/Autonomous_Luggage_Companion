28f27fe069c7e52183eec78873d087cfec66b574
Ksuck <pongchuti4@gmail.com>
2024-03-30 09:17:44 +0700

    follower bag

--- all_ai/src/bage_follwer.py
+++ all_ai/src/bage_follwer.py
@@ -20,8 +20,10 @@ class obj_pos() :
         rospy.Subscriber("/camera/color/image_raw", Image, self.rgb_callback)
         rospy.Subscriber("/camera/depth/image_raw", Image, self.depth_callback)
 
-        self.pos_arm = rospy.Publisher("/arm_xyz", Float64, queue_size = 10)
+        self.arm_turn = rospy.Publisher("/arm_turning", Float64, queue_size = 10)
+        self.arm_dist = rospy.Publisher("/arm_distange", Float64, queue_size = 10)
 
+        self.maximum_turning_speed = 15.0
 
         self.bridge = CvBridge()
         self.lock = threading.Lock()
@@ -60,7 +62,9 @@ class obj_pos() :
                     
                         self.lock.acquire()
                       
-                        xyz = self.get_obj_pos(self.frame_rgb , self.frame_depth)
+                        self.get_obj_pos(self.frame_rgb , self.frame_depth)
+
+                        #print(xy)
 
 
             
@@ -72,49 +76,70 @@ class obj_pos() :
                 
                 self.rate.sleep()
 
-            
-
 
         
-    def get_obj_pos(self, image, depth, get_leftmost = False, get_rightmost = False) :
+    def get_obj_pos(self, image, depth, get_leftmost = False, get_rightmost = True) :
         # image = cv2.resize(image, (320, 320))
         results = self.model.track(image, persist=True, verbose=False)
-        
+        height, width, _ = self.frame_rgb.shape
         boxes = results[0].boxes.xyxy.cpu()
         save_cen = [image.shape[1]/2, image.shape[0]/2]
         if results[0].boxes.id is not None:
-            for res in boxes  :
+            confs = results[0].boxes.conf.float().tolist()
+            confs = confs[0]
 
-                bb = res
+            #print(boxes)
+      
 
-                #print(bb)
-
-                leftmost = bb[0]
-                rightmost = bb[1]
-                topmost = bb[2]
-                bottommost = bb[3]
-
-
-
-                centroi = [int(rightmost - leftmost), int(bottommost - topmost)]
+            if confs > self.conf_threshold:
+                for bb in boxes :
                 
-                #print(centroi)
-                dist =  self.frame_depth[centroi[1], centroi[0]]
+                    #print(bb)
+    
+                    leftmost = bb[0]
+                    rightmost = bb[1]
+                    topmost = bb[2]
+                    bottommost = bb[3]
+    
+    
+    
+                    centroi = [int(rightmost - leftmost), int(bottommost - topmost)]
+                    
+    
+                    frame_ = results[0].plot()
+                    cv2.imshow("bag",frame_)
+    
+                    if get_leftmost and not get_rightmost :
+                      if centroi[0] < save_cen[0] :
+                          save_cen = centroi
+                    elif get_rightmost and not get_leftmost :
+                      if centroi[0] > save_cen[0] :
+                          save_cen = centroi
+                    else :
+                        save_cen = centroi
+                        break
+                    
+                    try:
+                        dist =  self.frame_depth[centroi[1], centroi[0]]
+                        self.arm_dist.publish(dist)
+    
+                    except Exception as e:
+                        rospy.logerr(e)    
+    
+                    try:
+                        pixel_dist = -save_cen[0] + (width/2)
+                        turning = (pixel_dist / width) * self.maximum_turning_speed
+    
+                        #print(turning)
+    
+                        self.arm_turn.publish(turning)
+                    except Exception as e:
+                        rospy.logerr(e)
+                
+            # arm_pos = self.cam_pos_to_cartesian_pos(*save_cen, dist , *self.FOV, *image.shape[:2])
+            # self.pos_arm.publish(arm_pos)
 
-                frame_ = results[0].plot()
-                cv2.imshow("bag",frame_)
-
-                if get_leftmost and not get_rightmost :
-                  if centroi[0] < save_cen[0] :
-                      save_cen = centroi
-                elif get_rightmost and not get_leftmost :
-                  if centroi[0] > save_cen[0] :
-                      save_cen = centroi
-                else :
-                    save_cen = centroi
-                    break
-            arm_pos = self.cam_pos_to_cartesian_pos(*save_cen, dist , *self.FOV, *image.shape[:2])
-            self.pos_arm.publish(arm_pos)
+            
 
     def cam_pos_to_cartesian_pos(self, px : int, py : int, dis : float, FOV_X : float, FOV_Y : float, image_width : int, image_height : int) -> list :
         """Converting camera position (Px, Py, Dist.) to cartesian position (X, Y, Z) relative on camera itself.
