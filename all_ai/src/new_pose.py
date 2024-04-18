2c8de9e2c5d5dd2175705cf9fcfc514c87a87026
ksuck <pongchuti4@gmail.com>
2024-04-18 04:06:12 -0700

    update naja

--- all_ai/src/new_pose.py
+++ all_ai/src/new_pose.py
@@ -3,26 +3,26 @@
 import rospy
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
+from std_msgs.msg import String, Int32 , Float64
+
 import cv2
+import numpy as np
+import threading
+
 import mediapipe as mp
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
-import threading
-import numpy as np
-from std_msgs.msg import String, Int32 , Float64
 
+from ultralytics import YOLO
 
 class hand_pose:
-    def __init__(self,cam_color,cam_depth, revers_cam = False , ros_rate = 500):
+    def __init__(self, cam_color, cam_depth, model_yolo = '', revers_cam = False, ros_rate = 500):
         #ชื่อ node
         rospy.init_node('hand_gesture_listener', anonymous=True)
         rospy.Subscriber(cam_color, Image, self.rgb_callback)
         rospy.Subscriber(cam_depth, Image, self.depth_callback)
-        
 
         self.bridge = CvBridge()
-
-        #เปิดเทรดให้ดึงภาพกล้องพร้อมกัน
         self.lock = threading.Lock()
 
         #กลับด้านกล้อง
@@ -31,7 +31,10 @@ class hand_pose:
         #ความเร็วการทำงานกล้อง
         self.rate = rospy.Rate(ros_rate)
 
-    #ดึงกล้อง rgb
+        self.model = YOLO(model_yolo)
+        
+
+
     def rgb_callback(self, data):
         try:
             frame_rgb = self.bridge.imgmsg_to_cv2(data, "bgr8")
@@ -44,7 +47,6 @@ class hand_pose:
         except Exception as e:
             rospy.logerr(e)
 
-    #ดึงกล้อง depth
     def depth_callback(self, data):
         try:
             frame_depth = self.bridge.imgmsg_to_cv2(data, desired_encoding="passthrough")
@@ -58,20 +60,38 @@ class hand_pose:
             rospy.logerr(e)
 
 
+
     def main_loop(self):
         while not rospy.is_shutdown():
             if hasattr(self, 'frame_rgb') and hasattr(self, 'frame_depth'):
                 try:
                     self.lock.acquire()
+                    results = self.model.track(self.frame_rgb)#จำกัดจำนวน [0]
+    
+                    #result_keypoint = results.keypoints.xyn.cpu().numpy()
 
+
+                    result_keypoint = results[0].keypoints.xyn.cpu().numpy()
+
+                    print(result_keypoint)
+                    
+                    frame_ = results[0].plot()
+
+
+                    cv2.imshow("Image",frame_)
+                    cv2.imshow("RGB", self.frame_rgb)
+                    
                     self.lock.release()
+                    cv2.waitKey(1)
                 except Exception as e:
                     rospy.logerr(e)
-            self.rate.sleep()
+
+                self.rate.sleep()
 
     def run(self):
-        threading.Thread(target=self.show_images).start()
+        threading.Thread(target=self.main_loop).start()
         rospy.spin()
+
 
 if __name__ == '__main__':
 
@@ -80,4 +100,9 @@ if __name__ == '__main__':
     topic_camera_color = '/camera/color/image_raw'
     topic_camera_depth = '/camera/depth/image_raw'
 
-    hand_pose(topic_camera_color,topic_camera_depth)
+    #ที่อยู่ url Ai 
+    yolov8_pose = 'buildnaja/src/Autonomous_Luggage_Companion/all_ai/src/yolov8n-pose.pt'
+    
+    build_naja = hand_pose(topic_camera_color, topic_camera_depth, yolov8_pose)
+    build_naja.run()
+    
